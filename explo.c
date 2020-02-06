41d7a203a1a8d36bf31028323bf300f4b8dc15f1
AlienorFache <59962539+AlienorFache@users.noreply.github.com>
2020-02-06 14:41:29 +0100

    Update explo.c

--- explo.c
+++ explo.c
@@ -3,27 +3,52 @@
 #include <time.h>
 #include <string.h>
 
-int main ()
+struct Lieux
 {
-	char pays1[10] = "Australie";
-	char pays2[6] = "Maroc";
-	
-	char paysChoix[10];
-	
+	char name[10];
+	char description[30];
+	int dif;
+	int tID[5];
+};
+typedef struct Lieux lieux;
+
+void deplacement (lieux lieu0, lieux lieu1, lieux lieu2)
+{
+	char lieuChoix[7];
 	
 	int ok = 0;
 	while (ok == 0)
 	{
-		printf("Voulez vous aller en Australie ou au Maroc ?\n");
-		scanf("%s",paysChoix);
+		printf("Ou voulez vous aller ?\n");
+		scanf("%s",lieuChoix);
 		
-		if (strcmp(paysChoix, pays1) == 0 || strcmp(paysChoix, pays2) == 0)
+		if (strcmp(lieuChoix, lieu0.name) == 0)
 		{
+			printf("Vous aller dans un endroit ou il y a %s.\n", lieu0.description);
+			ok ++;
+		}
+		if (strcmp(lieuChoix, lieu1.name) == 0)
+		{
+			printf("Vous aller dans un endroit ou il y a %s.\n", lieu1.description);
+			ok ++;
+		}
+		if (strcmp(lieuChoix, lieu2.name) == 0)
+		{
+			printf("Vous aller dans un endroit ou il y a %s.\n", lieu2.description);
 			ok ++;
 		}
 	}
+}
+
+int main ()
+{
+	lieux lieu0 = {"lieu1", "une foret tropical", 12,{1,2}};
+	lieux lieu1 = {"lieu2", "des montagnes", 5,{0,2}};
+	lieux lieu2 = {"lieu3", "une plage de sable blanc", 19,{0,1}};
 	
-	printf("Vous avez atteind : %s.\n", paysChoix);
+	
+	deplacement(lieu0, lieu1, lieu2);
+
 	
 	return 0;
 }
